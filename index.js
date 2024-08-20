b48ea39ffcecde46339caec524b7f5c80e7c7c0d
Shinji-Ikari95 <00001116813476SP@al.educacao.sp.gov.br>
2024-08-20 12:14:38 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("gray");
+  fill("yellow");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+  
+  let maximo = width;
+  let minimo = 0;
+  // mouseX, 0, width ==> 0, palavra.length
+  let palavra = "Nathan Fogli";
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+  
+  // if(mouseX < 50) {
+  //   let palavra = "N";
+  //   text(palavra, 200, 200);
+  // } else if(mouseX < 100) {
+  //   let palavra = "Na";
+  //   text(palavra, 200, 200);
+  // } else {
+  //   let palavra = "Nathan Fogli";
+  //   text(palavra, 200, 200);
+  // }
+
+}
+
